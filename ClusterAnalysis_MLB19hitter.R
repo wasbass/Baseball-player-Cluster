bdd80429ea448bc59d2eb4016c5c7624fb74526b
wasbass <waswang@gmail.com>
2021-07-02 02:17:26 +0800

    k-means finished

--- ClusterAnalysis_MLB19hitter.R
+++ ClusterAnalysis_MLB19hitter.R
@@ -1,4 +1,4 @@
-# Package
+##### Package#####
 {
   library(dplyr)
   library(magrittr)
@@ -9,15 +9,16 @@
   library(factoextra)
   library(corrgram)
   library(NbClust)
+  library(stats)
 }
 
 read.csv("C://RRR/19bat.csv",sep = "\t") -> bat19  #csv檔用Tab分隔的話，以\t輸入
 head(bat19)
 
 clearbat19 <- bat19[,c(4,11,14,15,16,17,18,20)] #挑選要用的變數 #17(長打率),18(上壘率)可考慮拿掉
-c2bat19    <- bat19[,c(4,11,14,15,16,      20)]
+#///c2bat19    <- bat19[,c(4,11,14,15,16,      20)]
 
-#建立敘述統計表格
+######建立敘述統計表格#####
 {
   clearbat19.ds <- data.frame(min  = c(1:8) ,
                               max  = c(1:8) ,
@@ -30,13 +31,14 @@ c2bat19    <- bat19[,c(4,11,14,15,16,      20)]
   as.data.frame(apply(clearbat19,2,sd  )) %>% round(2) -> clearbat19.ds$sd
 }
 
-#觀察共變數
+#####觀察共變數#####
 corrgram(clearbat19)
 pairs(clearbat19) #///lower.panel = NULL
 corrgram(c2bat19) #拿掉17,18看起來比較好
 #可以考慮用因素分析來拿掉高度線性相關的變數
 
-#用馬哈蘭距離(D^2)來偵測outlier(會用到變異數和共變數)
+#####馬哈蘭距離#####
+#用D^2來偵測outlier(會用到變異數和共變數)
 mh <- as.numeric(mahalanobis(clearbat19, colMeans(clearbat19), cov(clearbat19))) #以每個變數的平均數和共變矩陣為參數
 mh19 <- mutate(clearbat19,mh)
 mh19 %>%
@@ -44,6 +46,8 @@ mh19 %>%
   filter(mh>28)         #共有6個人，視為潛在離群值(99低打高長,371看不出,281盜壘最多,116看不出,315盜壘第二,424會轟會盜)
 
 df <- scale(clearbat19)
+
+#####主成分分析#####
 
 #用主成分分析的頭兩個主成分，來判斷集群分析是否有效，並且驗證分群結果的好壞
 fviz_pca_ind(prcomp(df), title = "PCA - clearbat19", palette = "jco",
@@ -81,7 +85,7 @@ order(bat.dist) %>%
 head(bat.dist)
 
 
-#依距離作區分原則的階層式集群分析
+#####分層式集群分析#####
 E.dist <- dist(clearbat19,method = "euclidean") #這邊用歐式距離當標準，也可以用manhattan等方法
 h.cluster <- hclust(E.dist , method = "ward.D2") #也可以用其他方法，像是最小距離、平均距離等
 #這邊用華德D2，會讓分群結果中，每一群的數量偏向更多
@@ -97,7 +101,7 @@ k3all  <- cbind(k3,bat19)
 hc2 <- agnes(E.dist, method = "ward")
 hc2$ac #agnes可以計算聚合係數(agglomerative coefficient)
 
-#Stopping Rule
+#####Stopping Rule#####
 stop_num <- NbClust(clearbat19, distance = "euclidean", method = "ward.D2", max.nc = 6, index = "all")
 stop_num
 #根據NbClust的數種Stopping Rule的Index顯示，3個分群應該是最好的選擇
@@ -120,7 +124,7 @@ stop_num
   }
   
   upper.plot <- function(x,y){
-    points( x , y , pch = 19 ,col = c("red","navyblue","darkgreen")[k3data$k3])  
+    points( x , y , pch = 19 ,col = c("red","navyblue","darkgreen")[kmdata$km])  #記得改
   }
   
   pairs(k3data, lower.panel = lower.cor , upper.panel = upper.plot )
@@ -148,4 +152,80 @@ k3data %>%
   group_by(k3) %>%
   tally() #各組人數
 
-#結論:第一組為普通人，第二組很會轟，第三組很會盜
+#分層式結論:第一組為普通人，第二組很會轟，第三組很會盜
+
+#####K-means#####
+km.bat <-kmeans( clearbat19 , centers = 3)
+km.bat #組間變異占總變異47.1%
+km.bat$withinss #各組的組內變異
+fviz_cluster(km.bat,           
+             data = clearbat19,              
+             geom = c("point","text"), 
+             frame.type = "norm") 
+
+
+#####調整組別#####
+change_group <-function(x){
+  if (x==1){ x = 2}
+  else if (x==2){ x = 1}
+  else {x = x}
+  return(x)
+}
+
+km <- km.bat$cluster
+km <- as.character(sapply(km,change_group))
+
+kmdata <- cbind(km,clearbat19)
+compare_data <-cbind(k3,kmdata)
+
+groupdif <- compare_data[,1:2]
+table(groupdif) #平庸組的人更平庸，巨砲組更嚴格，速度組差距不明顯
+
+pairs(kmdata, lower.panel = lower.cor , upper.panel = upper.plot )
+
+#####kmeans盒鬚圖#####
+boxplot(home_run ~ km , data = kmdata)
+
+#全壘打的組內差異更小了，代表分組的重要性變更大了
+ggplot(kmdata , aes(km,home_run)) +
+  geom_boxplot(aes(fill = km),notch = FALSE ) + ggtitle("home_run") + xlab("group")
+
+ggplot(k3data , aes(k3,home_run)) +
+  geom_boxplot(aes(fill = k3),notch = FALSE ) + ggtitle("home_run") + xlab("group")
+
+
+#相較之下，第三組盜壘數的變異被放大了
+
+ggplot(kmdata , aes(km,total_stolen_base)) +
+  geom_boxplot(aes(fill = km),notch = FALSE ) + ggtitle("total_stolen_base") + xlab("group")
+
+ggplot(k3data , aes(k3,total_stolen_base)) +
+  geom_boxplot(aes(fill = k3),notch = FALSE ) + ggtitle("total_stolen_base") + xlab("group")
+
+#另外，被三振率似乎也有關係，但相較之下不太重要
+ggplot(kmdata , aes(km, k_percent)) +
+  geom_boxplot(aes(fill = km),notch = FALSE ) + ggtitle("k_percent") + xlab("group")
+
+#可用來看各組變數分配的Cluster Profile
+ggparcoord(kmdata , columns = 2:9 , groupColumn = 1 ) #在資料數多的情況下不好用
+
+kmdata %>%
+  group_by(km) %>%
+  summarise(HR = mean(home_run), HRs = sd(home_run) , OBP = mean(on_base_percent) ,SB = mean(total_stolen_base) ,SBs = sd(total_stolen_base))
+
+kmdata %>%
+  group_by(km) %>%
+  tally() #各組人數
+
+#Stopping Rule
+fviz_nbclust(kmdata, 
+             FUNcluster = kmeans,   
+             method = "wss", 
+             k.max = 12) + 
+labs(title="Elbow Method for K-Means") +
+geom_vline(xintercept = 5,linetype = 2) #可以考慮分五群 
+
+#####最後可以考慮用Manova來檢驗各組的參數是否有顯著不同#####
+reg <- lm( as.numeric(km) ~ .  , data = kmdata)
+summary(reg)
+#這邊用LPM，可以看到顯著的有全壘打、盜壘和被三振率
